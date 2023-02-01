bd1272f09903419b56ba41069abfd25fc6b16203
Mathieu Méa <mathieu.mea@gmail.com>
2023-01-31 20:13:35 -0500

    Compatibility with latest update

--- src/main/java/org/mtransit/parser/fr_perpignan_sankeo_bus/PerpignanSankeoBusAgencyTools.java
+++ src/main/java/org/mtransit/parser/fr_perpignan_sankeo_bus/PerpignanSankeoBusAgencyTools.java
@@ -1,16 +1,23 @@
 package org.mtransit.parser.fr_perpignan_sankeo_bus;
 
+import static org.mtransit.commons.RegexUtils.DIGIT_CAR;
+import static org.mtransit.commons.RegexUtils.atLeastOne;
+import static org.mtransit.commons.RegexUtils.group;
+import static org.mtransit.commons.RegexUtils.mGroup;
+
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 import org.mtransit.commons.CharUtils;
 import org.mtransit.commons.CleanUtils;
 import org.mtransit.parser.DefaultAgencyTools;
+import org.mtransit.parser.MTLog;
 import org.mtransit.parser.gtfs.data.GStop;
 import org.mtransit.parser.mt.data.MAgency;
 import org.mtransit.parser.mt.data.MRouteSNToIDConverter;
 
 import java.util.List;
 import java.util.Locale;
+import java.util.regex.Pattern;
 
 // https://transport.data.gouv.fr/datasets/gtfs-sankeo
 public class PerpignanSankeoBusAgencyTools extends DefaultAgencyTools {
@@ -61,34 +68,46 @@ public class PerpignanSankeoBusAgencyTools extends DefaultAgencyTools {
 		return super.convertRouteIdNextChars(nextChars);
 	}
 
+	private static final Pattern SCHOOL_RID_ = Pattern.compile(
+			group(atLeastOne(DIGIT_CAR)) + "P" + group(atLeastOne(DIGIT_CAR))
+			, Pattern.CASE_INSENSITIVE);
+	private static final String SCHOOL_RID_REPLACEMENT = mGroup(1) + "000" + mGroup(2);
+
 	@Nullable
 	@Override
 	public Long convertRouteIdFromShortNameNotSupported(@NotNull String routeShortName) {
 		switch (routeShortName) {
 		case "A":
-			return MRouteSNToIDConverter.other(1L);
+			return 1000L;
 		case "B":
-			return MRouteSNToIDConverter.other(2L);
+			return 2000L;
 		case "C":
-			return MRouteSNToIDConverter.other(3L);
+			return 3000L;
 		case "CANET":
-			return MRouteSNToIDConverter.other(10L);
+			return 9000L;
 		case "CANET'ON D'ETE":
-			return MRouteSNToIDConverter.other(11L);
+			return 9001L;
 		case "NAV RAYONNANTES":
-			return MRouteSNToIDConverter.other(140L);
+			return 9002L;
 		case "NCMI":
-			return MRouteSNToIDConverter.other(141L);
+			return 9003L;
 		case "NAV DECOUVERTE":
-			return MRouteSNToIDConverter.other(142L);
+			return 9004L;
 		case "NITB":
-			return MRouteSNToIDConverter.other(143L);
+			return 9005L;
 		case "NCAN":
-			return MRouteSNToIDConverter.other(144L);
+			return 9006L;
 		case "NPTB":
-			return MRouteSNToIDConverter.other(145L);
+			return 9007L;
 		case "P'TIT BUS":
-			return MRouteSNToIDConverter.other(160L);
+			return 9008L;
+		case "NWIL":
+			return 9009L;
+		}
+		try {
+			return Long.parseLong(SCHOOL_RID_.matcher(routeShortName).replaceAll(SCHOOL_RID_REPLACEMENT));
+		} catch (NumberFormatException nfe) {
+			MTLog.logNonFatal(nfe);
 		}
 		return super.convertRouteIdFromShortNameNotSupported(routeShortName);
 	}
